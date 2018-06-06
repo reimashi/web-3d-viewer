8e48777142c3b6a2de7c638c79e4ddffea2a2c30
Aitor González Fernández <info@aitorgf.com>
2018-06-06 21:51:39 +0200

    Minor bug fixed

--- scripts/compile-and-run-windows.bat
+++ scripts/compile-and-run-windows.bat
@@ -7,7 +7,7 @@ REM Install chocolatey
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 REM Install dependencies
-choco install nodejs yarn git jdk8 maven -yarn
+choco install nodejs yarn git jdk8 maven -y
 
 yarn global add webpack webpack-cli
 
